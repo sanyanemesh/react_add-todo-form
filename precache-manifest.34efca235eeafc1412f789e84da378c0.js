6517aa187452076477b6c64151b4c3757477159d
sanyanemesh <sanya.nemesh@gmail.comgit config --global user.name sanyanemeshgit config --global user.email sanya.nemesh@gmail.com>
2020-05-04 15:34:31 +0300

    Updates

---
+++ precache-manifest.34efca235eeafc1412f789e84da378c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71032cd33eaa698bf8758bf26b9e0932",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b5d7db874d339b15a6a8",
+    "url": "./static/css/main.6e97d423.chunk.css"
+  },
+  {
+    "revision": "1588481fbb4cc36b1d7a",
+    "url": "./static/js/2.a3854a94.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.a3854a94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5d7db874d339b15a6a8",
+    "url": "./static/js/main.537440f0.chunk.js"
+  },
+  {
+    "revision": "23032c838e70e86a4a18",
+    "url": "./static/js/runtime-main.027178ab.js"
+  }
+]);
